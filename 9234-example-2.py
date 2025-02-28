7aa734df071888634edfe2c8fc045b71f1d68c96
Don Pugsley <donald.e.pugsley.civ@us.navy.mil>
2025-02-28 13:28:22 -0500

    Updated plotters.py, updated 9234-example-2 for Feb 2025 setup, working threeLSD plot

--- 9234-example-2.py
+++ 9234-example-2.py
@@ -6,58 +6,67 @@ import numpy as np
 import csv
 import time
 from datetime import datetime
+import pandas as pd
+import plotly.graph_objects as go
+import plotters as p
 
-sample_rate = 2048                              # Usually sample rate goes 2048 Hz, 2560 Hz, 3200 Hz, 5120 Hz, 6400 Hz (at least for a NI9234 card) 
-samples_to_acq = 2048                           # At least for vibration matters, Samples to acq go from 2048, 4096, 8192
+sample_rate = 51200                              # Usually sample rate goes 2048 Hz, 2560 Hz, 3200 Hz, 5120 Hz, 6400 Hz (at least for a NI9234 card) 
+samples_to_acq = 5120                           # At least for vibration matters, Samples to acq go from 2048, 4096, 8192
 wait_time = samples_to_acq/sample_rate          # Data Acquisition Time in s, very important for the NI task function, and since we have 2048 on both sides, time is 1 s
 cont_mode = AcquisitionType.CONTINUOUS              # There is also FINITE for sporadic measurements
 iterations = 10
     
 with nidaqmx.Task() as task:
-    now = datetime.now()
-    military = now.strftime('%H:%M:%S')     # Just recording the time like 20:32:54 instead of 8:32:54 pm
-    first_header = ['Some title in here']
-    second_header = [f'T. Captura: {military}']
+    # Create accelerometer channel and configure sample clock. current_excit_val=0.002 enables IEPE, velocity measured in ips and accel in m/s2, 
+    # both prox and tachometer can be used with a simple voltage channel.
+    # For more info about channels: https://nidaqmx-python.readthedocs.io/en/latest/ai_channel_collection.html
 
-# Create accelerometer channel and configure sample clock. current_excit_val=0.002 enables IEPE, velocity measured in ips and accel in m/s2, 
-# both prox and tachometer can be used with a simple voltage channel.
-# For more info about channels: https://nidaqmx-python.readthedocs.io/en/latest/ai_channel_collection.html
+    # accelerometer channels
+    task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ3Mod1/ai0", sensitivity = 100, current_excit_val = 0.002)
+    task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ3Mod1/ai1", sensitivity = 100, current_excit_val = 0.002)
+    task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ3Mod1/ai2", sensitivity = 100, current_excit_val = 0.002)
+    task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ3Mod1/ai3", sensitivity = 100, current_excit_val = 0.002)
 
-# Two accelerometer channels
-task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ9181-1E3866DMod1/ai0", sensitivity = 100, current_excit_val = 0.002)
-task.ai_channels.add_ai_accel_chan(physical_channel = "cDAQ9181-1E3866DMod1/ai1", sensitivity = 100, current_excit_val = 0.002)
+    # voltage channels
+    task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ3Mod2/ai0")
+    task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ3Mod2/ai1")
+    task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ3Mod2/ai2")
+    task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ3Mod2/ai3")
 
-# Two voltage channels
-task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ9181-1E3866DMod1/ai2")
-task.ai_channels.add_ai_voltage_chan(physical_channel = "cDAQ9181-1E3866DMod1/ai3")
+    total_wait_time = wait_time * iterations                         # We will only take 10 measurements, 10 s for this example
+    samples_to_acq_new = samples_to_acq * iterations                 # Also multiply by 10 to keep the same ratio, it should be 
 
-total_wait_time = wait_time * iterations                         # We will only take 10 measurements, 10 s for this example
-samples_to_acq_new = samples_to_acq * iterations                 # Also multiply by 10 to keep the same ratio, it should be 
-
-# Sets source of sample clock, its rate, and number of samples to aquire = buffer size
-task.timing.cfg_samp_clk_timing(sample_rate, sample_mode = cont_mode, samps_per_chan = samples_to_acq_new)               
-  
-start = time.time()
-print ('Starting task...')         # Just to keep a control in your console
-
-# Saving the 4 channels to a csv file. This file is overwritten everytime the program is executed.
-# It should appear in the same folder that your program is located. 
-with open('data_10times.csv', 'w', newline = '') as f:
-    writer = csv.writer(f)
-    writer.writerow(first_header)
-    writer.writerow(second_header)
+    # Sets source of sample clock, its rate, and number of samples to aquire = buffer size
+    task.timing.cfg_samp_clk_timing(sample_rate, sample_mode = cont_mode, samps_per_chan = samples_to_acq_new)               
     
-    # adding some blank spaces in btw
-    writer.writerow('')
-    writer.writerow('')        
-    
-    x = np.linspace(0, total_wait_time, samples_to_acq_new)       # Your x axis (ms), starts from 0, final time is total_wait_time, equally divided by the number of samples you'll capture
-    
-    data = np.ndarray((4, samples_to_acq_new), dtype = np.float64)  #Creates an array, 4 columns each one of 20480 rows
-    nidaqmx.stream_readers.AnalogMultiChannelReader(task.in_stream).read_many_sample(data, samples_to_acq_new, timeout = 14) # it should't take that long for this example, check out time for other exercises               
-    
-    for value in range(len(x)):
-        writer.writerow([x[value], data[0][value], data[1][value], data[2][value], data[3][value]])
+    start = time.time()
+    print ('Starting task...')         # Just to keep a control in your console
 
-elapsed_time = (time.time() - start)
-print (f'done in {elapsed_time}')
+    # Saving the 4 channels to a csv file. This file is overwritten everytime the program is executed.
+    # It should appear in the same folder that your program is located. 
+    with open('data.csv', 'w', newline = '') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Time','Ax','Ay','Az','Mic','Mx','My','Mz','NC'])
+        t = np.linspace(0, total_wait_time, samples_to_acq_new)       # Your x axis (ms), starts from 0, final time is total_wait_time, equally divided by the number of samples you'll capture
+        
+        data = np.ndarray((8, samples_to_acq_new), dtype = np.float64)  #Creates an array, 4 columns each one of 20480 rows
+        stream_readers.AnalogMultiChannelReader(task.in_stream).read_many_sample(data, samples_to_acq_new, timeout = 14) # it should't take that long for this example, check out time for other exercises               
+        
+        for value in range(len(t)):
+            writer.writerow([t[value], data[0][value], data[1][value], data[2][value], data[3][value],data[4][value], data[5][value], data[6][value], data[7][value]])
+
+    elapsed_time = (time.time() - start)
+    print (f'done in {elapsed_time}')
+
+df = pd.read_csv('data.csv')
+
+# Plotly version - displays in browser
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x = df['Time'], y = df['Ax'], mode='lines', name='Ax'))
+# fig.add_trace(go.Scatter(x = df['Time'], y = df['Ay'], mode='lines', name='Ay'))
+# fig.add_trace(go.Scatter(x = df['Time'], y = df['Az'], mode='lines', name='Az'))
+# fig.add_trace(go.Scatter(x = df['Time'], y = df['Mic'], mode='lines', name='Mic'))
+
+p.threelsd(df['Ax'],df['Ay'],df['Az'],51200,'g','9234 Accel','Ax','Ay','Az',6)
+
+
--- plotters.py
+++ plotters.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 29 08:40:15 2022
-
-@author: pugsley
-
-Analysis plot functions
-"""
+# Library of plotting and analysis functions
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy import signal
+from math import sqrt
 
 def threeplot(t,x,y,z,units,title):
     fig, ax = plt.subplots()
@@ -25,8 +19,41 @@ def threeplot(t,x,y,z,units,title):
     ax.set_xlabel('Time (seconds)', fontsize=12)
     plt.tick_params(axis='both', which='major', labelsize=12)
     # plt.axis([0, 1100, 0, 1100000])
-    plt.grid(visible='true')
-    plt.show(block=False)
+    plt.grid(visible=True)
+    plt.show()
+
+def ftmg7plot(t,G,units,title):
+    fig, ax = plt.subplots()
+    # Extract components... zx and zy are redundant
+    xx = G[0,0,:]
+    xy = G[0,1,:]
+    xz = G[0,2,:] 
+    yx = G[1,0,:]
+    yy = G[1,1,:]
+    yz = G[1,2,:]
+    zz = G[2,2,:]
+    mxx = np.mean(xx)
+    mxy = np.mean(xy)
+    mxz = np.mean(xz) 
+    myx = np.mean(yx)
+    myy = np.mean(yy)
+    myz = np.mean(yz) 
+    mzz = np.mean(zz) 
+    ax.plot(t, xx-mxx,label=f'XX {mxx:.0f} DC')
+    ax.plot(t, xy-mxy,label=f'XY {mxy:.0f} DC')
+    ax.plot(t, xz-mxz,label=f'XZ {mxz:.0f} DC')
+    ax.plot(t, yx-myx,label=f'YX {myx:.0f} DC')
+    ax.plot(t, yy-myy,label=f'YY {myy:.0f} DC')
+    ax.plot(t, yz-myz,label=f'YZ {myz:.0f} DC')
+    ax.plot(t, zz-mzz,label=f'ZZ {mzz:.0f} DC')
+    ax.legend()
+    ax.set_title(title)
+    ax.set_ylabel(units)
+    ax.set_xlabel('Time (seconds)', fontsize=12)
+    plt.tick_params(axis='both', which='major', labelsize=12)
+    # plt.axis([0, 1100, 0, 1100000])
+    plt.grid(visible=True)
+    plt.show()
 
 def tf(x,y,z):
     return np.sqrt(x*x+y*y+z*z)
@@ -41,25 +68,24 @@ def oneplot(t,v,units,title):
     ax.set_xlabel('Time (seconds)', fontsize=12)
     plt.tick_params(axis='both', which='major', labelsize=12)
     # plt.axis([0, 1100, 0, 1100000])
-    plt.grid(visible='true')
-    plt.show(block=False)
+    plt.grid(visible=True)
+    plt.show()
 
 def lsd(t,v,sr,units,title):
     freqs, psd = signal.welch(v,fs=sr,nperseg=len(v)//2)
     lsd = np.sqrt(psd) 
-    # lsfreqs = freqs[1]+freqs[1:] # Duplicate first nonzero point, LS doesn't like 0 freq
-    # lspsd = signal.lombscargle(t,v,lsfreqs/(2*np.pi)) # Convert from Hz to rads/sec
-    # lslsd = np.sqrt(lspsd)
+    lsfreqs = freqs[1]+freqs[1:] # Duplicate first nonzero point, LS doesn't like 0 freq
+    lspsd = signal.lombscargle(t,v,lsfreqs/(2*np.pi)) # Convert from Hz to rads/sec
+    lslsd = np.sqrt(lspsd)
     plt.figure(figsize=(5, 4))
     plt.loglog(freqs, lsd, label='Welch')
-    # plt.loglog(lsfreqs, lslsd, label='Lomb-Scargle')
-    # plt.legend()
+    plt.loglog(lsfreqs, lslsd, label='Lomb-Scargle')
     plt.title('LSD  '+title)
     plt.xlabel('Frequency (Hz)')
     plt.ylabel(units+'/rtHz')
     plt.tight_layout()
-    plt.grid(visible='true')
-    plt.show(block=False)
+    plt.grid(visible=True)
+    plt.show()
 
 def threelsd(x,y,z,sr,units,title,lx,ly,lz,welchfactor):
     xfreqs, xpsd = signal.welch(x,fs=sr,nperseg=len(x)//welchfactor)
@@ -69,16 +95,16 @@ def threelsd(x,y,z,sr,units,title,lx,ly,lz,welchfactor):
     zfreqs, zpsd = signal.welch(z,fs=sr,nperseg=len(z)//welchfactor)
     lsdz = np.sqrt(zpsd)
     plt.figure(figsize=(5, 4))
-    plt.loglog(xfreqs,lsdx, label='X')
-    plt.loglog(yfreqs,lsdy, label='Y')
-    plt.loglog(zfreqs,lsdz, label='Z')
+    plt.loglog(xfreqs,lsdx, label=lx)
+    plt.loglog(yfreqs,lsdy, label=ly)
+    plt.loglog(zfreqs,lsdz, label=lz)
     plt.legend()
     plt.title('LSD  '+title)
     plt.xlabel('Frequency (Hz)')
     plt.ylabel(units+'/rtHz')
     plt.tight_layout()
-    plt.grid(visible='true')
-    plt.show(block=False)
+    plt.grid(visible=True)
+    plt.show()
 
 def sg(v,sr,units,title):
     freqs, times, spectrogram = signal.spectrogram(v,fs=sr)
@@ -89,24 +115,25 @@ def sg(v,sr,units,title):
     plt.xlabel('Time')
     plt.tight_layout()
     plt.ylim([0, sr/2])
-    plt.show(block=False)
+    plt.show()
 
-# import matplotlib as mpl
-# def scattermap(lats,lons,title):
-#    import gmplot
-#    #  Charminar_top_attraction_lats, Charminar_top_attraction_lons = zip(*[
-#    # (17.3833, 78.4011),(17.4239, 78.4738),(17.3713, 78.4804),(17.3616, 78.4747),
-#    # (17.3578, 78.4717),(17.3604, 78.4736),(17.2543, 78.6808),(17.4062, 78.4691),
-#    # (17.3950, 78.3968),(17.3587, 78.2988),(17.4156, 78.4750)])
+# Reference paper: Xu, Lei, et al. "Simulation analysis of magnetic gradient full-tensor measurement system.",
+#   Mathematical Problems in Engineering 2021 (2021): 1-13.
+# Their formulations use 1..n sensor designation, kept that here to avoid transcription errors
+    
+def ftmgcross(d,B1x,B1y,B1z,B2x,B2y,B2z,B3x,B3y,B3z,B4x,B4y,B4z):
+    # Calculate FTMG for four-sensor cross with separation d
+    # Sensor layout:    2
+    #                 3   1
+    #                   4
+    return (1/d)*[[B1x-B3x, B1y-B3y, B1z-B3z],[B2x-B4x, B2y-B4y, B2z-B4z], [B1z-B3z, B2z-B4z, B3x+B4y-(B1x+B2y)]]
 
-#    #declare the center of the map, and how much we want the map zoomed in
-#    gmap3 = gmplot.GoogleMapPlotter(17.3616, 78.4747, 13)
-#    # Scatter map
-#    gmap3.scatter( lats, lons, '#FF0000',size = 50, marker = False )
-#    # Plot method Draw a line in between given coordinates
-#    gmap3.plot(lats, lons, 'cornflowerblue', edge_width = 3.0)
-#    #Your Google_API_Key
-#    gmap.apikey = " API_Key”
-#    # save it to html
-#    gmap3.draw(r"/tmp/temporaryscatter.html")
-
+def ftmget(d,B1x,B1y,B1z,B2x,B2y,B2z,B3x,B3y,B3z):
+    # Calculate FTMG for equilateral triangle with side length d
+    # Sensor layout:    2
+    #                 3   1
+    return np.asarray(1/sqrt(3)*d)*[
+        [sqrt(3)*(B1x-B3x), sqrt(3)*(B1y-B3y), sqrt(3)*(B1z-B3z)],
+        [2*B2x-(B1x+B3x), 2*B2y-(B1y+B3y), 2*B2z-(B1z+B3z)],
+        [sqrt(3)*(B1z-B3z), 2*B2z-(B1z+B3z), (B1y+B3y)-2*B2y-sqrt(3)*(B1x-B3x)]]
+    
